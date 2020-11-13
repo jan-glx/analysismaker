16b741b7e2233b43f9f727bd4666559a4d6c464d
Jan Gleixner <jan.gleixner@gmail.com>
2020-11-13 21:48:56 +0100

    extend test to second notebook with dependency

--- tests/testthat/test-make_notebooks.R
+++ tests/testthat/test-make_notebooks.R
@@ -2,7 +2,8 @@ context("simple checks")
 
 test_that("creating simple analysis + makefile does not error", {
   analysis_1 <- new_analysis(name = "test_analysis_1")
-  analysis_1 %<>% add_notebook("test_notebook_1.Rmd")
+  analysis_1 %<>% add_notebook("test_notebook_1.Rmd", products = c("result_1" = "test_dep_file.txt"))
+  analysis_1 %<>% add_notebook("test_notebook_2.Rmd", dependencies = c("example_dep_1" = "result_1"))
   analysis_1 %>% write_makefile()
   expect_true(TRUE)
   expect_equal(system("make"), 0)

412a6f7f925e0f7b926ab74c1994eeb85c005afb
Jan Gleixner <jan.gleixner@gmail.com>
2020-11-13 21:30:26 +0100

    add test testing makefile

--- tests/testthat/test-make_notebooks.R
+++ tests/testthat/test-make_notebooks.R
@@ -1,7 +1,9 @@
 context("simple checks")
 
 test_that("creating simple analysis + makefile does not error", {
-  analysis_1 <- new_analysis(name = "test_analysis_1", notebook_dir = ".")
+  analysis_1 <- new_analysis(name = "test_analysis_1")
   analysis_1 %<>% add_notebook("test_notebook_1.Rmd")
   analysis_1 %>% write_makefile()
+  expect_true(TRUE)
+  expect_equal(system("make"), 0)
 })

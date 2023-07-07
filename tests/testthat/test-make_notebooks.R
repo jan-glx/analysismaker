1f935a00e4c0ed6086fd8d896b5503dc301cebaa
Jan Gleixner <jan.gleixner@gmail.com>
2023-07-07 14:39:26 +0200

    switch to testthat 3e

--- tests/testthat/test-make_notebooks.R
+++ tests/testthat/test-make_notebooks.R
@@ -1,9 +1,7 @@
-context("simple checks")
-
 test_that("creating simple analysis + makefile does not error", {
   analysis_1 <- new_analysis(name = "test_analysis_1")
-  analysis_1 %<>% add_notebook("test_notebook_1.Rmd", products = c("result_1" = "test_dep_file.txt"))
-  analysis_1 %<>% add_notebook("test_notebook_2.Rmd", dependencies = c("example_dep_1" = "result_1"))
+  suppressMessages(analysis_1 %<>% add_notebook("test_notebook_1.Rmd", products = c("result_1" = "test_dep_file.txt")))
+  suppressMessages(analysis_1 %<>% add_notebook("test_notebook_2.Rmd", dependencies = c("example_dep_1" = "result_1")))
   analysis_1 %>% write_makefile()
   expect_true(TRUE)
   make_output <- system2("make", stdout = TRUE)

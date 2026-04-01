59c6b8d3da4c4e8a4253b9421210f0c1bb26ca22
Jan Gleixner <jan.gleixner@gmail.com>
2026-04-01 18:03:05 +0200

    improve platformindependence of testing

--- tests/testthat/test-make_notebooks.R
+++ tests/testthat/test-make_notebooks.R
@@ -7,7 +7,7 @@ test_that("creating simple analysis + makefile does not error", {
   make_output <- system2("make", stdout = TRUE)
   Sys.sleep(1)
   expect_equal(attr(make_output, "status"), NULL)
-  expect_equal(system2("make", stdout = TRUE), "make: Nothing to be done for 'test_analysis_1'.")
+  expect_equal(substr(system2("make", stdout = TRUE), 1, 14), substr("make: Nothing to be done for `test_analysis_1'.", 1, 14))
   fs::file_delete("makefile")
   fs::file_delete("test_analysis_1.mk")
   fs::dir_delete("results")

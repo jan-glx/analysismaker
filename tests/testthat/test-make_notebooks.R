1c96bb6fed3e2fa7734996bf2bf8a7471ed9add3
Jan Gleixner <jan.gleixner@gmail.com>
2026-04-17 11:36:43 +0200

    Remove manual cleanup in favor of withr temporary directory

--- tests/testthat/test-make_notebooks.R
+++ tests/testthat/test-make_notebooks.R
@@ -1,16 +1,15 @@
 simple_analysis <- make_simple_analysis()
 
 test_that("makefile generation does not error", {
+  wd <- withr::local_tempdir()
+  file.copy(test_path("notebooks"), wd, recursive = TRUE)
+  withr::local_dir(wd)
+
   write_makefile(simple_analysis, analysis_name = "test_analysis_1")
   expect_true(TRUE)
   make_output <- system2("make", stdout = TRUE)
   expect_equal(attr(make_output, "status"), NULL)
   Sys.sleep(1)
   expect_equal(substr(system2("make", stdout = TRUE), 1, 14), substr("make: Nothing to be done for `test_analysis_1'.", 1, 14))
-  # Cleanup
-  fs::file_delete("makefile")
-  fs::file_delete("test_analysis_1.mk")
-  fs::dir_delete("results")
-  fs::dir_delete("results_human")
 })
 

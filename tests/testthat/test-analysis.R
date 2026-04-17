bf969572b7166506afa1b50eae6bf33ec8c37274
Jan Gleixner <jan.gleixner@gmail.com>
2026-04-17 11:30:02 +0200

    Add analysis tests and refactor makefile test

---
+++ tests/testthat/test-analysis.R
@@ -0,0 +1,4 @@
+test_that("creating simple analysis does not error", {
+  make_simple_analysis()
+  expect_true(TRUE)
+})
--- tests/testthat/test-make_notebooks.R
+++ tests/testthat/test-make_notebooks.R
@@ -1,11 +1,13 @@
-test_that("creating simple analysis + makefile does not error", {
-  analysis_1 <- make_simple_analysis(name = "test_analysis_1")
-  analysis_1 %>% write_makefile()
+simple_analysis <- make_simple_analysis()
+
+test_that("makefile generation does not error", {
+  write_makefile(simple_analysis, analysis_name = "test_analysis_1")
   expect_true(TRUE)
   make_output <- system2("make", stdout = TRUE)
-  Sys.sleep(1)
   expect_equal(attr(make_output, "status"), NULL)
+  Sys.sleep(1)
   expect_equal(substr(system2("make", stdout = TRUE), 1, 14), substr("make: Nothing to be done for `test_analysis_1'.", 1, 14))
+  # Cleanup
   fs::file_delete("makefile")
   fs::file_delete("test_analysis_1.mk")
   fs::dir_delete("results")

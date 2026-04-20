c48f3579719812ca81e47783d8819434ffb706f3
Jan Gleixner <jan.gleixner@gmail.com>
2026-04-20 18:15:46 +0200

    add dry run tests for large analysis

--- tests/testthat/test-analysis.R
+++ tests/testthat/test-analysis.R
@@ -1,4 +1,12 @@
 test_that("creating simple analysis does not error", {
-  make_simple_analysis()
-  expect_true(TRUE)
+  expect_no_error(make_simple_analysis())
+})
+
+test_that("creating small large analysis does not error", {
+  expect_no_error(make_large_analysis(n=2))
+})
+
+test_that("creating large analysis does not error", {
+  expect_no_error(make_large_analysis(n=2))
+  expect_no_error(make_large_analysis())
 })
